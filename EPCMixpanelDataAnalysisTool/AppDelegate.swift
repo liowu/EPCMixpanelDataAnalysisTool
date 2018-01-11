af6a099c0f1fe3ecc13d1ce16f1a02ca8b76b36e
liowu <v-liowu@expedia.com>
2018-01-11 15:48:31 +0800

    init

---
+++ EPCMixpanelDataAnalysisTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EPCMixpanelDataAnalysisTool
+//
+//  Created by LioWu on 11/01/2018.
+//  Copyright © 2018 Expedia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EPCMixpanelDataAnalysisTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EPCMixpanelDataAnalysisTool
+//
+//  Created by LioWu on 11/01/2018.
+//  Copyright © 2018 Expedia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
